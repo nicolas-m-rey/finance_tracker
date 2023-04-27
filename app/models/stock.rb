aa4afd6d085e1e7f6e4bc8547110ce140313da39
nicolas-m-rey <nic.m.rey@gmail.com>
2023-04-27 09:28:45 -0700

    Fixed API code

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,10 +1,10 @@
 class Stock < ApplicationRecord
 
-  def self.new_lookup()
+  def self.new_lookup(stock)
     client = IEX::Api::Client.new(publishable_token: 'pk_e68ccc999f3c4a3d8851b5919c8c04b8',
                                   endpoint: 'https://cloud.iexapis.com/v1')
   
-    client.get('tops?symbols=snap', token: 'sk_c027ef096f864ea8b729e694e6a18470')
+    client.get("tops?symbols=#{stock}", token: 'sk_c027ef096f864ea8b729e694e6a18470')
   end
   
 end
