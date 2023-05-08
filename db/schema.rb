b5f21d8151e54eb19f2dc15d30ff7389dc0964d4
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-08 13:29:35 -0700

    Migration for stock table rename

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_08_183506) do
+ActiveRecord::Schema.define(version: 2023_05_08_201532) do
 
   create_table "stocks", force: :cascade do |t|
-    t.string "ticker"
-    t.string "name"
+    t.string "stock"
+    t.string "sector"
     t.decimal "last_price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
