0bccf77d0a89bed7412424fa7cbd51da0d8ea8a5
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-10 11:55:47 -0700

    Cleaned up navbar and added some friend functionality.

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
    root 'home#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stock#search'
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
 
   
 end
