668641fb11c3ec2e431757880766190239b97699
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-10 10:35:14 -0700

    Added friendships table and relations.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,12 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
-  before_actrion :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
   respond_to :html, :json, :js 
 
   protected
 
   def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
     devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
   end
 end
