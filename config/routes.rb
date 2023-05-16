4222ee5ea587883f4bf50d6fa11ea9ce878b0921
nicolas-m-rey <nic.m.rey@gmail.com>
2023-05-16 10:27:45 -0700

    We go againe againe

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
-  resources :users, only: [:show]
+  # resources :users, only: [:show]
   resources :friendships, only: [:create, :destroy]
 
   devise_for :users
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stock#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
-
+  get 'users/:id', to: 'users#show', as: 'user'
 
   
 end
